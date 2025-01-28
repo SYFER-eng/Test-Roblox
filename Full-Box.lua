ec6e0adf57b2f9494227c9c4cce848b9ba708161
Syfer-eng <reviewedpuppy93@gmail.com>
2025-01-27 18:57:03 -0600

    Create Full-Box.lua

---
+++ Full-Box.lua
@@ -0,0 +1,8 @@
+local Sense = loadstring(game:HttpGet('https://raw.githubusercontent.com/SYFER-eng/Test-Roblox/refs/heads/main/Box.lua'))()
+
+
+Sense.teamSettings.enemy.enabled = true
+Sense.teamSettings.enemy.box = true
+Sense.teamSettings.enemy.boxColor[1] = Color3.new(0, 0.25, 0.75)
+
+Sense.Load()
